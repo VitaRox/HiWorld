4a5e58e74a59b6d6f8839a6df49d7110586f03c0
VitaRox <24485205+VitaRox@users.noreply.github.com>
2019-10-13 16:05:19 -0700

    Initial commit
    First commit: add java and compiled file, as well as IntelliJ project file

---
+++ HiWorld.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+
+public class HiWorld {
+
+    public static void main(String[] args) {
+        String userIn = " ";
+        String userOut = " ";
+
+        while(!userIn.equals("done")) {
+
+            try {
+                InputStreamReader isr = new InputStreamReader(System.in);
+                BufferedReader br = new BufferedReader(isr);
+                System.out.println("Please enter some shit: ");
+                userIn = br.readLine();
+
+                if (userIn.equals("done")) {
+                    System.out.println(userOut);
+                } else {
+                    userOut = userOut.concat(userIn);
+                }
+
+            } catch (IOException ioe) {
+                System.err.println("Whoops!");
+            }
+        }
+    }
+}
+
