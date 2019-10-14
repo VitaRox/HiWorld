01314cc86cc947262cea6bd3e55f2aa08b25cb5f
Vita <wiebetc@gmail.com>
2019-10-14 15:51:08 -0700

    modified: HiWorld.java - arbitrarily remove JavaDoc comments at the top

--- HiWorld.java
+++ HiWorld.java
@@ -1,12 +1,7 @@
 import java.io.*;
 
 
-/* A simple class designed to take in user input via the console until
- * user is finished, at which point they enter 'done' and the program
- * prints out a string of the entire input the user typed.
- * @author VitaRox
- * @version 1.0
- */
+
 public class HiWorld {
 
     public static void takeUserInput() {
