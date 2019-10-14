6e2fac0ca485c2191364e3ab68066cd7068dfac8
Vita <wiebetc@gmail.com>
2019-10-14 15:54:47 -0700

    -Add output introducing the user output once program is about to exit.

--- HiWorld.java
+++ HiWorld.java
@@ -1,7 +1,11 @@
 import java.io.*;
 
-
-
+/* A simple class designed to take in user input via the console until
+ * user is finished, at which point they enter 'done' and the program
+ * prints out a string of the entire input the user typed.
+ * @author VitaRox
+ * @version 1.0
+ */
 public class HiWorld {
 
     public static void takeUserInput() {
@@ -17,6 +21,7 @@ public class HiWorld {
                 userIn = br.readLine();
 
                 if (userIn.equals("done")) {
+                    System.out.println("Here is the probably-vulgar nonsense you typed: ");
                     System.out.println(userOut);
                 } else {
                     userOut = userOut.concat(userIn);
